1a259a36f51f5c4a0fffde4ee16bc47c6de5b591
jkvyff <kristianvyff@gmail.com>
2020-06-15 22:08:17 -0700

    Updates

---
+++ precache-manifest.73471413763021da5d18cc8b8dabe974.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaba4544615d1e13b9c",
+    "url": "/insquare/b01a141043c4e882012e.module.wasm"
+  },
+  {
+    "revision": "0fb53751a492a325e093a42e07a1936c",
+    "url": "/insquare/index.html"
+  },
+  {
+    "revision": "30fa302b14c32c3b15ab",
+    "url": "/insquare/static/css/main.1e4908e7.chunk.css"
+  },
+  {
+    "revision": "f5e3a6c305a4c0be6b9e",
+    "url": "/insquare/static/js/2.aa303074.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/insquare/static/js/2.aa303074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaba4544615d1e13b9c",
+    "url": "/insquare/static/js/3.66dcf01f.chunk.js"
+  },
+  {
+    "revision": "30fa302b14c32c3b15ab",
+    "url": "/insquare/static/js/main.9e97bb52.chunk.js"
+  },
+  {
+    "revision": "b029b8b9acea9a0178d3",
+    "url": "/insquare/static/js/runtime-main.66ac4626.js"
+  }
+]);
